1ce2104e70582587ca295093a9f0087b567065a1
Joana-Pacheco <pacheco.jo86@gmail.com>
2026-02-10 16:53:07 -0300

    Atualizando o código

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log ("Olá mundão");
+let nomeHeroi = "Artemis;
+let xpHeroi = 7500;
+let nivel = "";
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel ="Bronze";
+}
